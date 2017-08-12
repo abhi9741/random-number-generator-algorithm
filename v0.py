58c51cc6353fcfff177599ba1738ea792b6a7ec5
Abhinav Reddy Nimma <abhinavreddynimma@gmail.com>
2017-08-12 20:57:08 +0530

    basic algorithm
    the basic/first algorithm using already available functions / libraries

---
+++ v0.py
@@ -0,0 +1,14 @@
+
+from random import randint as r
+#random library has an easy partern of showing random integers that can be easily cracked
+def random_int():
+        k=r(1,9)	
+	zk=(7*k)%11
+	return zk
+
+#random ibrary is used to generate a random no. which inturn is used in our formula to generate a random integer , this makes the pattern of showing random no. more efficient
+
+
+
+
+
